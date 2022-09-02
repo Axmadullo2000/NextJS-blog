12d680dadd7269b74b4275538f4d08530cf77cc1
axmadullo2001TATU <axmadullo2000@gmail.com>
2022-09-02 17:32:01 +0300

    NextJS Blog

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import MainContainer from "./components/MainContainer";
+import MainContainer from "../components/MainContainer";
 
 export default function NotFound() {
     return (
